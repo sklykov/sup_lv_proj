dddd4f3d4def8441da1884ec629e3d80552ac954
ssklykov <ssklykov@yandex.ru>
2020-11-23 16:03:07 +0100

    Transfer through interim image tested

---
+++ Save_interim_img/Open_saved_tiff.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Open the saved interim tiff file and measure performance.
+
+@author: Serega
+"""
+# %% Imports
+from PIL import Image
+import numpy as np
+import time
+
+# %% Tests
+start = time.time()
+img = Image.open("img.tif", mode='r')
+img = np.array(img)
+finish = time.time()
+print("Operation takes:", round(1000*(finish - start), 1), "ms")
---
+++ UDP_save_img/scripts/img_read_udp.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Test capabilties of image transfer through saving it to the disk, reading and subsequent deleting.
+
+@author: ssklykov
+"""
+# %% Imports
+from PIL import Image
+import numpy as np
+import time
+import socket
+
+# %% Script-wide parameters
+host = 'localhost'
+port_py_main = 5005  # number of the first ports of that will be opened
+port_LV = 5100
+st_n_bytes = 1024
+
+# %% UDP server as listener for LabVIEW commands
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as mainServer:
+    mainServer.bind((host, port_py_main))
+    flag = True
+    print("Python UDP Server launched")
+
+    while flag:
+        (command, address) = mainServer.recvfrom(st_n_bytes)
+        command = str(command, encoding='utf-8')  # Conversion to a normal string
+        if "Ping" in command:
+            print(command, '- received command')
+            sendingString = "Echo"
+            sendingString = sendingString.encode()  # to utf-8
+            mainServer.sendto(sendingString, address)
+
+        elif "Image saved" in command:
+            print(command, '- received command')
+            img = Image.open("img.tif", mode='r')
+            img = np.array(img)
+            print("Image readed and stored in this script")
+            sendingString = "Image transferred".encode()
+            mainServer.sendto(sendingString, address)
+            # For simple check that some image transferred - perform some operation on an image
+            print("Mean pixel value:", round(np.mean(img)))
+
+        elif "QUIT" in command:
+            print(command, '- received command')
+            # Do the quiting action with a delay
+            sendingString = "Quit performed".encode()
+            mainServer.sendto(sendingString, address)
+            time.sleep(0.15)  # A delay for preventing of closing connection automatically by Python (causing errors)
+            flag = False
+            break
+
+
+# %% For correctly representation of an image - it should be performed after quiting of image transfer
+# For testing if the transfer of an image perfromed correctly
+try:
+    if np.size(img, axis=0) > 0:
+        from skimage import io
+        from skimage.util import img_as_ubyte
+        import matplotlib.pyplot as plt
+        if np.max(img) < 256:
+            img = np.uint8(img)
+        img_show = img_as_ubyte(img)
+        plt.figure("Transferred image")
+        io.imshow(img_show)
+except NameError:
+    print("No image transferred")
